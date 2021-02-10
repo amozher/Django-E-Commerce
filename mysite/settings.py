a2b9cf450a7af972243aa1de05ec8d175d097faf
ammmmozher <mozher.abdeslam@gmail.com>
2021-02-11 00:36:38 +0100

    rrr

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,12 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #Ajouter from heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles','home','static')
-STATIC_URL = 'home/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'home/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/uploads/'
